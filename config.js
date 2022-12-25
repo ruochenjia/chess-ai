0d29ad15d9eee8cd34774159c8b212e0e46b22c1
Ruochen Jia <rjia@rjia.gq>
2022-12-25 13:55:12 +0000

    í 

---
+++ config.js
@@ -0,0 +1,16 @@
+
+const config = {
+	server: "https://nxdomain.chromehack.com/",
+	debug: true,
+	cacheName: "chesscheta",
+	cacheVersion: "0.3.0",
+	cacheList: [],
+	headers: {
+		"Cross-Origin-Embedder-Policy": "require-corp",
+		"Cross-Origin-Opener-Policy": "same-origin",
+		"Referrer-Policy": "origin-when-cross-origin",
+		"X-Content-Type-Options": "nosniff",
+	}
+};
+
+export default config;
